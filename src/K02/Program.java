0928904cb8204e1a61433088ff38662dd3da0697
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2022-12-13 11:02:28 +0100

    README Update

--- src/K02/Program.java
+++ src/K02/Program.java
@@ -1,9 +1,9 @@
 package K02;
 
-public class Program {
-    public static void main(String[] args){
-        Menu menu = new Menu();
-        menu.haupt();
-    }
-    }
+    public class Program {
+        public static void main(String[] args){
+            Menu menu = new Menu();
+            menu.haupt();
+        }
+        }
 
